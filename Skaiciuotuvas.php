4593f2d57b520468d8dc054535b81e916e5a8989
EvaldasMatiukas <matiukas.e@gmail.com>
2017-06-28 21:13:44 +0300

    Change
    Divide change to div

---
+++ Skaiciuotuvas.php
@@ -0,0 +1,54 @@
+
+<?php
+
+	//$argument1 = $_GET['arg1'];
+	//$argument2 = $_GET['arg2'];
+	//echo $argument1;
+	
+	if(isset($_GET['arg1']) && $_GET['arg1'] != null) {
+
+		switch($_GET['operation']) {
+			case "add":
+				$result = intval($_GET['arg1']) + intval($_GET['arg2']);
+			break;
+			case "sub":
+				$result = intval($_GET['arg1']) - intval($_GET['arg2']);
+			break;
+			case "mul":
+				$result = intval($_GET['arg1']) * intval($_GET['arg2']);
+			break;
+			case "div":
+				$result = intval($_GET['arg1']) / intval($_GET['arg2']);
+			break;
+
+		}
+
+		echo $result;
+	} else {
+		echo "No argument 1";
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculator</title>
+</head>
+<body>
+	<form action=>
+		<input type="text" name="arg1">
+		<select name="operation">
+		 	<option value="add">Add</option>
+		 	<option value="sub">Subract</option>
+		 	<option value="mul">Multiply</option>
+		 	<option value="div">Divide</option>
+		</select>
+		<input type="text" name="arg2">
+		<input type="submit" value="Calculate!">
+
+	</form>
+
+
+</body>
+</html>
